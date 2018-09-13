c0120e2b7c92bdcbaf0797a79e728379a66fbf58
Nursan amar <nursan011@gmail.com>
2018-09-13 22:47:33 +0800

    change route

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
       //   <Text>Changes you make will automatically reload.</Text>
       //   <Text>Shake your phone to open the developer menu.</Text>
       // </View>
-      <HomeStack/>
+      <MainStack/>
     );
   }
 }
