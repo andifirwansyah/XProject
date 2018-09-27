f27828a26a71445cce531f363b10c0ec1d1d0d7d
Andi WawanK <andifirwansyah250@gmail.com>
2018-09-27 08:51:14 +0700

    split component #1

---
+++ src/Components/CardWisata.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import {
+    StyleProvider,
+    Container,
+    Content,
+    Card,
+    CardItem,
+    Body
+} from 'native-base';
+
+class CardWisata extends Component{
+    render(){
+        return(
+            <Content>
+                {/* -> Child Component */}
+            </Content>
+        )
+    }
+}
+export default CardWisata;
